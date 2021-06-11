5d7a3688cc175932dbafad621ab50a21ce6d56d0
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-06-11 11:02:42 +0200

    added validation methods to users endpoint

--- model.js
+++ model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 let movieSchema = mongoose.Schema({
   Title: { type: String, required: true },
   Description: { type: String, required: true },
@@ -16,6 +17,15 @@ let userSchema = mongoose.Schema({
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectID, ref: "Movie" }]
 });
 
+//hashPassword function does the actual hashing of submitted passwords.
+userSchema.statics.hashPassword = password => {
+  return bcrypt.hashSync(password, 10);
+};
+
+//validatePassword, compares submitted hashed passwords with the hashed passwords stored in database.
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 let actorSchema = mongoose.Schema({
   Name: { type: String, required: true },
   Bio: { type: String, required: true },
